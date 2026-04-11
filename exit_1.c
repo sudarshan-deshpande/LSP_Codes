bee4d251a4dddd92478f8fe4b045f1a426472e52
sudarshandeshpande2-cmyk <sudarshandeshpande2@gmail.com>
2026-04-11 23:48:10 +0530

    11-04-2026

---
+++ exit_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Process is created\n");
+
+    return 0; // exit(0)
+}
---
+++ exit_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    printf("Process is created\n");
+
+    exit(0); // Good Programming practice
+}
